cba3121e8b758a8078924f4372d7ecbc22ff3cf4
ogotech-global <okoyecharles1990@gmail.com>
2023-10-06 09:37:19 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is the function of this task
+ * Return: o
+ */
+
+int main(void)
+
+{
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+putchar('\n');
+
+return (0);
+}
