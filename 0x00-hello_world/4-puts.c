7180d911fcf90b054cf9cb9165632329f0edbed0
ogotech-global <okoyecharles1990@gmail.com>
2023-10-08 14:01:20 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main -Writing my first C program that prints a line with puts function
- * Return: 0
+ * main -Entry point of C Program
+ * Return: Always 0 =success
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
