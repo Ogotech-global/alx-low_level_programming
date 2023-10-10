6708584fbad96e7db786786d95e40ee8237c3caf
ogotech-global <okoyecharles1990@gmail.com>
2023-10-10 16:16:29 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,10 +2,11 @@
 /**
  * print_sign - Prints the sign of a number
  * @n: The number to be checked
- * Returns: 1 and prints + if n is greater than zero,
+ * Return: 1 and prints + if n is greater than zero,
  *	0 and prints 0 if n is zero,
  *	-1 and prints - if n is less than zero
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
@@ -23,4 +24,5 @@ int print_sign(int n)
 		_putchar('-');
 		return (-1);
 	}
+		_putchar('\n');
 }
