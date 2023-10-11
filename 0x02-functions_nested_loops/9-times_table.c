46ec756733768931a71ba33eb31895733af10c27
ogotech-global <okoyecharles1990@gmail.com>
2023-10-11 05:39:41 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,7 @@ void times_table(void)
 					_putchar(' ');
 				else
 					_putchar((result / 10) + '0');
-				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
 			}
 		}
 		_putchar('\n');
