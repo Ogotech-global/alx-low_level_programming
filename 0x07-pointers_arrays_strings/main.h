39be2db9d7ec9ca42737a64154ee2aa415d04631
ogotech-global <okoyecharles1990@gmail.com>
2023-10-23 17:45:25 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <unistd.h>
 
+void print_diagsums(int *a, int size);
+void print_number(int n);
+void print_char(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
