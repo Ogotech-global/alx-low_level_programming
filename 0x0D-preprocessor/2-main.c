ec575b83b31d5a8251061623e9dcbe0435bbf4bf
ogotech-global <okoyecharles1990@gmail.com>
2023-11-06 09:47:33 -0800

    this is task 2-main.c under preprocessor project

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ */
 
 int main(void)
 {
-    printf("%s\n", __FILE__);
-    return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
-
