7dfd49144684f1fe482d5ced06933071da191bfb
ogotech-global <okoyecharles1990@gmail.com>
2023-11-08 16:21:22 -0800

    Task 1 update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -10,37 +10,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
 }
-
-/**
- * print_name_as_is - Prints a name as is.
- * @name: The name to be printed.
- *
- * Return: Nothing.
- */
-void print_name_as_is(char *name)
-{
-	printf("Hello, my name is %s\n", name);
-}
-
-/**
- * print_name_uppercase - Prints a name in uppercase.
- * @name: The name to be printed.
- *
- * Return: Nothing.
- */
-void print_name_uppercase(char *name)
-{
-	while (*name != '\0')
-	{
-		if (*name >= 'a' && *name <= 'z')
-			putchar(*name - 32);
-		else
-			putchar(*name);
-		name++;
-	}
-	putchar('\n');
-}
-
