76aebd1bf585a561d5cf7bd95e4cbe23e651693f
Your Name <okoyecharles1990@gmail.com>
2023-11-16 19:22:17 -0800

    update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,25 +1,9 @@
-section .data
-	hello db 'Hello, Holberton', 0xA ; String to be printed with a newline character
-
-section .text
-	global _start
-
-_start:
-	; Write the string to stdout using write syscall
-	; Syscall number for write is 1
-	mov rax, 1
-	; File descriptor for stdout is 1
-	mov rdi, 1
-	; Address of the string
-	mov rsi, hello
-	; Length of the string
-	mov rdx, 18 ; 17 characters + 1 newline
-	syscall
-
-	; Exit the program using exit syscall
-	; Syscall number for exit is 60
-	mov rax, 60
-	; Exit code 0
-	xor rdi, rdi
-	syscall
-
+global	main
+	extern printf
+main:
+	mov edi, format
+	xor eax, eax
+	call printf
+	mov	eax, 0
+	ret
+format: db 'Hello, Holberton', 0
