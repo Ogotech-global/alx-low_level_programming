a7c3ce3f03fde25c86a04224f626639eb68195a4
Your Name <okoyecharles1990@gmail.com>
2023-11-16 19:40:37 -0800

    update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,17 @@
-global	main
-	extern printf
+section	.data
+	; Declare data section
+	format db 'Hello, Holberton', 0 ; Null-terminated string
+
+section	.text
+	; Declare text section
+	global main ; Entry point for the program
+	extern printf ; Declare printf function
+
 main:
-	mov edi, format
-	xor eax, eax
-	call printf
-	mov	eax, 0
-	ret
-format: db 'Hello, Holberton', 0
+	; Start of the main function
+	mov edi, format ; Move the address of the format string to edi register
+	xor eax, eax ; Clear eax register (set it to 0)
+	call printf ; Call printf function
+	mov eax, 0 ; Set the return value to 0 (exit code)
+	ret ; Return from the main function
+
